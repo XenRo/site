6871a5883c4b1fc079a819b7ca7fa7f090d57c0d
XenRon <XenRo@users.noreply.github.com>
2016-10-01 17:48:43 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ var options = {
   start:"autostart"
 };
 
-var vivus = new Vivus('logo-diagonal-right','logo-diagonal-left', options, onComplete);
+var vivus = new Vivus('logo-diagonal-right', options, onComplete);
 
 function onComplete() {}
+
+var vivus = new Vivus('logo-diagonal-left', options, onComplete);
