2ceb2c6e3cfcd12c6ba245b2eb7ae58dd7f1e1f7
XenRon <XenRo@users.noreply.github.com>
2016-10-02 11:02:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,10 +41,7 @@ var vivus = new Vivus('logo-corner-down', options, onComplete);
 function onComplete() {}
 
 //show page only when fully loaded
-    //<![CDATA[
-        $(window).on('load', function() { // makes sure the whole site is loaded 
-            $('#status').fadeOut(); // will first fade out the loading animation 
-            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-            $('body').delay(350).css({'overflow':'visible'});
-        })
-    //]]>
+$(window).load(function() {
+  // When the page has loaded
+  $("html").fadeIn(2000);
+});
